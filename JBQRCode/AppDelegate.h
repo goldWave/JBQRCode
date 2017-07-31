7f17c1db408dbf4411e25724441347104452eeed
renjinbo <renjinbo@navercorp.com>
2017-07-31 17:25:13 +0800

    Initial Commit

---
+++ JBQRCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JBQRCode
+//
+//  Created by jimbo on 2017/7/31.
+//  Copyright © 2017年 naver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
